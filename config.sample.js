fb126fdee4bbf0518ccbd0afe76678904da9d19a
Greg Allen <me@jga.me>
2011-06-16 15:32:48 -0700

    updated config sample

--- config.sample.js
+++ config.sample.js
@@ -1,9 +1,83 @@
-modules.exports = {
-  loggly: {
-    subdomain: '',
+module.exports = {
+  '{subdomain name}': {
     auth: {
       username: '',
       password: ''
+    },
+    widgets: {
+      traffic: {
+        title: 'Page Views Today',
+        type: 'facet',
+        facet: 'date',
+        query: 'status=200',
+        ui: {
+          type: 'bar',
+          dateFormat: 'HH:mm',
+          autoRefresh: true
+        },
+        context: {
+          from: 'NOW-5HOURS',
+          gap: '+10MINUTES'
+        }
+      },
+      redirects: {
+        title: 'Redirects',
+        type: 'facet',
+        facet: 'date',
+        query: 'status=301',
+        ui: {
+          type: 'bar',
+          dateFormat: 'HH:mm',
+          autoRefresh: true
+        },
+        context: {
+          from: 'NOW-5HOURS',
+          gap: '+10MINUTES'
+        }
+      },
+      notFound: {
+        title: 'Not Found',
+        type: 'facet',
+        facet: 'date',
+        query: 'status=404',
+        ui: {
+          type: 'bar',
+          dateFormat: 'HH:mm',
+          autoRefresh: true
+        },
+        context: {
+          from: 'NOW-5HOURS',
+          gap: '+10MINUTES'
+        }
+      },
+      recentPages: {
+        title: 'Recent Pages',
+        type: 'search',
+        query: 'request',
+        regex: 'url=(.*?),',
+        ui: {
+          type: 'list',
+          dateFormat: 'MM/dd h:mm tt',
+          autoRefresh: true
+        },
+        context: {
+          from: 'NOW-1DAY'
+        }
+      },
+      topReferrals: {
+        title: 'Referrals',
+        type: 'search',
+        query: 'ref=http',
+        regex: 'ref=(.*?)(,|$)',
+        ui: {
+          type: 'list',
+          dateFormat: 'MM/dd h:mm tt',
+          autoRefresh: true
+        },
+        context: {
+          from: 'NOW-1DAY'
+        }
+      }
     }
   }
 };
